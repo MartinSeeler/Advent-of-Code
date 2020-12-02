9410e8163450125ae9ca76a6dc16318f89c7281b
Martin Seeler <developer@chasmo.de>
2020-12-02 07:19:31 +0100

    Add solutions for day02 part 2

--- day02/day02.py
+++ day02/day02.py
@@ -16,3 +16,18 @@ def is_entry_valid(range, pattern, text):
 
 valid_results = reduce(lambda sum, entry: sum + 1 if is_entry_valid(*entry) else sum, input_list, 0)
 print(f"Valids: {valid_results}")
+
+def is_entry_valid_part2(range, pattern, text):
+    [first_idx, second_idx] = [int(x) for x in range.split("-")]
+    p = re.compile(pattern[:-1])
+    matches_first_idx = matches_second_idx = False
+    for m in re.finditer(p, text):
+        match_idx = m.start() + 1
+        if match_idx == first_idx:
+            matches_first_idx = True
+        elif match_idx == second_idx:
+            matches_second_idx = True
+    return (matches_first_idx or matches_second_idx) and not (matches_first_idx and matches_second_idx)
+
+valid_results_2 = reduce(lambda sum, entry: sum + 1 if is_entry_valid_part2(*entry) else sum, input_list, 0)
+print(f"Valids Part Two: {valid_results_2}")
