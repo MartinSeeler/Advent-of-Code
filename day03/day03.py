64a5e0bbbbadbdb05dbfecaa545c8a103f465977
Martin Seeler <developer@chasmo.de>
2020-12-03 06:51:23 +0100

    Add solution for day03 part 2

--- day03/day03.py
+++ day03/day03.py
@@ -1,18 +1,21 @@
-import re
+from operator import mul
 from functools import reduce
 
 input_list = []
 with open("./input.txt", "r") as f:
     input_list = f.read().splitlines()
-    
 
-x = 0
-y = 0
-trees = 0
-while y < len(input_list):
-    print(x, y, trees)
-    trees += 1 if input_list[y][x] == "#" else 0
-    x = (x+3) % len(input_list[0])
-    y += 1
 
-print("Final", x, y, trees)
+def get_trees(x_step, y_step):
+    x = 0
+    y = 0
+    trees = 0
+    while y < len(input_list):
+        trees += 1 if input_list[y][x] == "#" else 0
+        x = (x+x_step) % len(input_list[0])
+        y += y_step
+    return trees
+
+
+slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+print("Final", reduce(mul, [get_trees(*params) for params in slopes]))
