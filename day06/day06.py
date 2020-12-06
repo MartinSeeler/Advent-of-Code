ad1fae9e862aeee4696e467404cfc17f0ac9786b
Martin Seeler <developer@chasmo.de>
2020-12-06 07:30:59 +0100

    Add solution for day06 part 2

--- day06/day06.py
+++ day06/day06.py
@@ -1,5 +1,12 @@
+import string
+from functools import reduce
+
 total = 0
 with open("./input.txt", "r") as f:
     for input_group in f.read().split("\n\n"):
-        total += len(set("".join(input_group.split("\n"))))
+        total += sum(map(len, reduce(
+            set.intersection, 
+            map(set, input_group.split("\n")), 
+            set(string.ascii_lowercase)
+        )))
 print(total)
