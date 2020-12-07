ee5fd499d6dfde488834f4b304240f8c852dca04
Martin Seeler <developer@chasmo.de>
2020-12-07 06:53:56 +0100

    Add solution for day07 part 2

--- day07/day07.py
+++ day07/day07.py
@@ -30,9 +30,18 @@ def path_exists(current_color, target_color):
         return res
     else:
         return False
-final = 0
-for c in all_colors:
-    if c != target_color:
-        final += path_exists(c, target_color)
 
+# solution part 1
+#final = 0
+#for c in all_colors:
+#    if c != target_color:
+#        final += path_exists(c, target_color)
+
+def calc(current_color):
+    if current_color in paths:
+        return sum(map(lambda x: int(x[0]) + (int(x[0]) * calc(x[1])), paths[current_color]))
+    else:
+        return 0
+
+final = calc(target_color)
 print(final)
