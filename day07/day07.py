b546eb7dc6eab2b78b7b24362265ac7a0c21bc43
Martin Seeler <developer@chasmo.de>
2020-12-07 06:40:11 +0100

    Add solution for day07 part 1

---
+++ day07/day07.py
@@ -0,0 +1,38 @@
+import re
+
+rules_list = []
+with open("./input.txt", "r") as f:
+    rules_list = f.read().splitlines()
+
+paths = dict()
+all_colors = []
+
+for rule in rules_list:
+    [head, tail] = rule.split(" contain ")
+    match_head = re.match(r"^(\w+\s\w+)\sbag[s]?.*", head, re.I)
+    c = match_head.groups()[0]
+    all_colors.append(c)
+    if match_head:
+        match_tail = re.findall(r"(\d)\s(\w+\s\w+)\sbag[s]?", tail, re.I)
+        if match_tail:
+            paths[c] = match_tail
+print(paths)
+target_color = "shiny gold"
+
+def path_exists(current_color, target_color):
+    if current_color in paths:
+        res = False
+        for xs in paths[current_color]:
+            if xs[1] == target_color:
+                return True
+            print("checking", xs, "for", current_color)
+            res = res or path_exists(xs[1], target_color)
+        return res
+    else:
+        return False
+final = 0
+for c in all_colors:
+    if c != target_color:
+        final += path_exists(c, target_color)
+
+print(final)
