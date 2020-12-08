66865e985401757c8d18d0aad1f6302be83c2f4c
Martin Seeler <developer@chasmo.de>
2020-12-08 06:42:03 +0100

    Change day08 solution

--- day08/day08.py
+++ day08/day08.py
@@ -2,19 +2,21 @@ il = []
 with open("./input.txt", "r") as f:
     il = f.read().splitlines()
 
+parse = lambda idx: int(il[idx].split(" ")[1])
+
 def fs(idx, acc, visited):
     if idx in visited:
         return None
     elif idx == len(il):
         return visited, acc
     else:
-        if il[idx].startswith("nop"):
-            return (fs(idx+1, acc, visited.union(set([idx]))) or 
-                fs(idx+int(il[idx].split(" ")[1]), acc, visited.union(set([idx]))))
-        elif il[idx].startswith("acc"):
-            return fs(idx+1, acc+int(il[idx].split(" ")[1]), visited.union(set([idx])))
+        if il[idx].find("nop")==0:
+            return (fs(idx+1, acc, visited | set([idx])) or 
+                fs(idx+parse(idx), acc, visited | set([idx])))
+        elif il[idx].find("acc")==0:
+            return fs(idx+1, acc+parse(idx), visited | set([idx]))
         else:
-            return (fs(idx+int(il[idx].split(" ")[1]), acc, visited.union(set([idx]))) or 
-                fs(idx+1, acc, visited.union(set([idx]))))
+            return (fs(idx+parse(idx), acc, visited | set([idx])) or 
+                fs(idx+1, acc, visited | set([idx])))
 
 print(fs(0, 0, set())[1])
