758fda4316c72596362d4f81208ea9f7f0bc3fae
Martin Seeler <developer@chasmo.de>
2020-12-08 06:33:53 +0100

    Change day08 solution

--- day08/day08.py
+++ day08/day08.py
@@ -2,17 +2,19 @@ il = []
 with open("./input.txt", "r") as f:
     il = f.read().splitlines()
 
-def find_solution(idx, acc, visited):
+def fs(idx, acc, visited):
     if idx in visited:
         return None
-    if idx == len(il):
+    elif idx == len(il):
         return visited, acc
     else:
         if il[idx].startswith("nop"):
-            return find_solution(idx+1, acc, visited.union(set([idx]))) or find_solution(idx+int(il[idx].split(" ")[1]), acc, visited.union(set([idx])))
+            return (fs(idx+1, acc, visited.union(set([idx]))) or 
+                fs(idx+int(il[idx].split(" ")[1]), acc, visited.union(set([idx]))))
         elif il[idx].startswith("acc"):
-            return find_solution(idx+1, acc+int(il[idx].split(" ")[1]), visited.union(set([idx])))
+            return fs(idx+1, acc+int(il[idx].split(" ")[1]), visited.union(set([idx])))
         else:
-            return find_solution(idx+int(il[idx].split(" ")[1]), acc, visited.union(set([idx]))) or find_solution(idx+1, acc, visited.union(set([idx])))
+            return (fs(idx+int(il[idx].split(" ")[1]), acc, visited.union(set([idx]))) or 
+                fs(idx+1, acc, visited.union(set([idx]))))
 
-print(find_solution(0, 0, set())[1])
+print(fs(0, 0, set())[1])
