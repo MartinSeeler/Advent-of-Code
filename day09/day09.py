50ed007ec15bf0099e9926f5e241d76b77bd4bc2
Martin Seeler <developer@chasmo.de>
2020-12-09 06:33:03 +0100

    Change day09 solution

--- day09/day09.py
+++ day09/day09.py
@@ -1,25 +1,20 @@
-from itertools import combinations
-
-nums = []
-with open("./input.txt", "r") as f:
-    nums = list([int(x) for x in f.read().splitlines()])
+from itertools import combinations as cs
 
 pa_length = 25
-invalid_num = -1
-for part in [nums[i:i+pa_length+1] for i in range(0,len(nums), 1)]:
-    xmas = part[:pa_length]
-    target = part[-1]
-    res = set()
-    for i in combinations(xmas, 2):
-        if sum(i) == target:
-            res.add(i)
-    if len(res) == 0:
-        print(target)
-        invalid_num = target
-        break
+with open("./input.txt", "r") as f:
+    nums = list([int(x) for x in f.read().splitlines()])
+    for part in [nums[i:i+pa_length+1] for i in range(0,len(nums), 1)]:
+        xmas = part[:pa_length]
+        target = part[-1]
+        combis = set()
+        for i in cs(xmas, 2):
+            if sum(i) == target:
+                combis.add(i)
+        if len(combis) == 0:
+            print("Part1", target)
+            for n in range(2,len(nums)-1):
+                for xs in [nums[i:i+n] for i in range(0,len(nums), 1)]:
+                    if sum(xs) == target:
+                        print("Part2", min(xs)+max(xs))
+                        exit(-1)
 
-for n in range(2,100):
-    for part in [nums[i:i+n] for i in range(0,len(nums), 1)]:
-        if sum(part) == invalid_num:
-            print(min(part)+max(part))
-            exit(-1)
