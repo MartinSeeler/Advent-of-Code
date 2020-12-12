892f7dbce0d4c1f011f4a64e8c2f548830cd3b15
Martin Seeler <developer@chasmo.de>
2020-12-12 16:18:22 +0100

    Change day06 to even shorter answer

--- day06/day06.py
+++ day06/day06.py
@@ -1,10 +1,8 @@
-import string
-from functools import reduce
+from functools import reduce as r
+from operator import or_, and_
 
-total = 0
 with open("./input.txt", "r") as f:
-    print(sum([sum(map(len, reduce(
-            set.intersection, 
-            map(set, xs.split("\n")), 
-            set(string.ascii_lowercase)
-        ))) for xs in f.read().split("\n\n")]))
+    xs = [x.split("\n") for x in "".join(f.read()).split("\n\n")]
+    solve = lambda fn: sum(len(r(fn, map(set, x))) for x in xs)
+    print("Part 1:", solve(or_))
+    print("Part 2:", solve(and_))
