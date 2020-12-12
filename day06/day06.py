752b61c5d4e10bdda7889746587b453a335a5b6b
Martin Seeler <developer@chasmo.de>
2020-12-12 16:20:20 +0100

    Change day06 to even shorter answer++

--- day06/day06.py
+++ day06/day06.py
@@ -2,7 +2,7 @@ from functools import reduce as r
 from operator import or_, and_
 
 with open("./input.txt", "r") as f:
-    xs = [x.split("\n") for x in "".join(f.read()).split("\n\n")]
-    solve = lambda fn: sum(len(r(fn, map(set, x))) for x in xs)
-    print("Part 1:", solve(or_))
-    print("Part 2:", solve(and_))
+    xs = [x.split("\n") for x in f.read().split("\n"*2)]
+    s = lambda fn: sum(len(r(fn, map(set, x))) for x in xs)
+    print("Part 1:", s(or_))
+    print("Part 2:", s(and_))
