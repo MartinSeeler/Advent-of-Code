c6f82d53002c5a1c87425b101b3a0447b143d6d8
Martin Seeler <developer@chasmo.de>
2020-12-13 20:07:52 +0100

    Add template for new days

---
+++ template/solution.py
@@ -0,0 +1,13 @@
+def solve_part_1(text: str):
+  pass
+
+
+def solve_part_2(text: str):
+  pass
+
+
+if __name__ == '__main__':
+  with open("input.txt", "r") as f:
+    quiz_input = f.read()
+    print("Part 1:", solve_part_1(quiz_input))
+    print("Part 2:", solve_part_2(quiz_input))
