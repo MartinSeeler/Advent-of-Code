699f5c09638af31545ead49fdc1608aca52f3aa1
Martin Seeler <developer@chasmo.de>
2020-12-13 16:26:00 +0100

    Change format of part 2 solution of day 13

--- day13/day13_2.py
+++ day13/day13_2.py
@@ -1,16 +1,17 @@
-from functools import reduce
-from math import lcm, prod
+from math import prod
+
 
 def solve(inp):
-	bus_deps = [(int(x), idx) for idx, x in enumerate(inp.split(",")) if x != "x"]
-	# Extended Euclidean algorithm
-	# https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-	invm = lambda a, b: 0 if a==0 else 1 if b%a==0 else b - invm(b%a,a)*b//a
-	# chinese remainder theorems
-	# https://en.wikipedia.org/wiki/Chinese_remainder_theorem
-	N = prod([bs[0] for bs in bus_deps])
-	x = sum([bs[1]*(N//bs[0])*invm(N//bs[0], bs[0]) for bs in bus_deps])
-	return N - x % N
+  bus_deps = [(int(x), idx) for idx, x in enumerate(inp.split(",")) if x != "x"]
+  # Extended Euclidean algorithm
+  # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+  invm = lambda a, b: 0 if a == 0 else 1 if b % a == 0 else b - invm(b % a, a) * b // a
+  # chinese remainder theorems
+  # https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+  N = prod([bs[0] for bs in bus_deps])
+  x = sum([bs[1] * (N // bs[0]) * invm(N // bs[0], bs[0]) for bs in bus_deps])
+  return N - x % N
+
 
 with open("./input.txt", "r") as f:
-	print(solve(f.read().splitlines()[1]))
+  print(solve(f.read().splitlines()[1]))
