cd6a6e56b676432b4758890aee88bd3787042763
Martin Seeler <developer@chasmo.de>
2020-12-15 07:02:10 +0100

    Lambdas rulezs whoamielf

--- 2020/day15/solution.py
+++ 2020/day15/solution.py
@@ -3,15 +3,14 @@ def solve(text: str, limit: int):
   turn = len(nums) + 1
   memory = {x: [n + 1] for n, x in enumerate(nums)}
   last = nums[-1]
+  insert = lambda l, m, t: [t] if l not in m else [m[l][-1]] + [t]
   while turn <= limit:
-    if turn % 10000 == 0:
-      print(turn, last)
     if last in memory and len(memory[last]) == 1:
       last = 0
-      memory[last] = [turn] if last not in memory else [memory[last][-1]] + [turn]
+      memory[last] = insert(last, memory, turn)
     elif last in memory and len(memory[last]) > 1:
       last = memory[last][-1] - memory[last][-2]
-      memory[last] = [turn] if last not in memory else [memory[last][-1]] + [turn]
+      memory[last] = insert(last, memory, turn)
     else:
       memory[last] = [turn]
     turn += 1
