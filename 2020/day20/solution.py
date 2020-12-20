cb5f43a0427b8c675de254a51dd8da13ac68bc00
Martin Seeler <developer@chasmo.de>
2020-12-20 09:26:05 +0100

    Add first steps and fixed tests for day20 part2

--- 2020/day20/solution.py
+++ 2020/day20/solution.py
@@ -1,21 +1,37 @@
-from math import prod
+from math import prod, sqrt
+
+transpose = lambda tile: list(''.join(line) for line in zip(*tile))
+invert = lambda tile: [''.join(reversed(line)) for line in tile]
+rotate = lambda tile: invert(transpose(tile))
 
 
-def solve_part_1(text: str):
-  d = dict()
+def parse(text: str):
+  tiles = dict()
+  tile_sides = dict()
   for block in text.split("\n\n"):
     lines = block.splitlines()
     sides = [lines[1], lines[-1], "".join(l[0] for l in lines[1:]), "".join(l[-1] for l in lines[1:])]
     sides += [s[::-1] for s in sides]
-    d[int(lines[0].split(" ")[1][:-1])] = sides
-  e = dict()
-  for k, sides in d.items():
-    e[k] = set.union(*map(set, list([j for j in d.keys() if side in d[j] and j != k] for side in sides)))
-  return prod([k for k, sides in e.items() if len(sides) == 2])
+    id = int(lines[0].split(" ")[1][:-1])
+    tile_sides[id] = sides
+    tiles[id] = lines[1:]
+  adjacents = dict()
+  for k, sides in tile_sides.items():
+    adjacents[k] = set.union(
+      *map(set, list([j for j in tile_sides.keys() if side in tile_sides[j] and j != k] for side in sides)))
+  return tiles, adjacents
+
+
+def solve_part_1(text: str):
+  _, adjacents = parse(text)
+  return prod([k for k, sides in adjacents.items() if len(sides) == 2])
 
 
 def solve_part_2(text: str):
-  pass
+  tiles, adjacents = parse(text)
+  N = int(sqrt(len(tiles)))
+  board = [[None] * N for _ in range(N)]
+  print(rotate(tiles[3079]))
 
 
 if __name__ == '__main__':
