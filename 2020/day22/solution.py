fd8cd6dbcdb7dd69780d36356f24461d54931d10
Martin Seeler <developer@chasmo.de>
2020-12-22 06:48:34 +0100

    Add solution for day22 part2 (whoopsie)

--- 2020/day22/solution.py
+++ 2020/day22/solution.py
@@ -1,3 +1,8 @@
+from collections import Counter
+import sys
+
+sys.setrecursionlimit(100500)
+
 def parse_cards(text: str) -> ([], []):
   return list(list(xs.splitlines()[1:]) for xs in text.split("\n\n"))
 
@@ -19,6 +24,38 @@ def play_round(p1, p2):
     return play_round(p1[1:], p2[1:] + [p2[0]] + [p1[0]])
 
 
+def play_round_rec(p1, p2, c=Counter()):
+  if len(p1) == 0:
+    return 1, p2
+  if len(p2) == 0:
+    return 0, p1
+  round_key = f"{', '.join(p1)}-{', '.join(p2)}"
+  if c[round_key] > 0:
+    print(f"Round key {round_key} was met before")
+    return 0, p1
+  c[round_key] += 1
+  print(f"Player 1's deck: {', '.join(p1)}")
+  print(f"Player 2's deck: {', '.join(p2)}")
+  print(f"Player 1 plays: {p1[0]}")
+  print(f"Player 2 plays: {p2[0]}")
+  if (len(p1[1:]) >= int(p1[0])) and (len(p2[1:]) >= int(p2[0])):
+    print("Playing sub round")
+    winner_id, winner_deck = play_round_rec(p1[1:int(p1[0])+1], p2[1:int(p2[0])+1], Counter())
+    print("Winner rec", winner_id)
+    if winner_id:
+      print("Player 2 wins the round!")
+      return play_round_rec(p1[1:], p2[1:] + [p2[0]] + [p1[0]], c)
+    else:
+      print("Player 1 wins the round!")
+      return play_round_rec(p1[1:] + [p1[0]] + [p2[0]], p2[1:], c)
+  elif int(p1[0]) > int(p2[0]):
+    print("Player 1 wins the round!")
+    return play_round_rec(p1[1:] + [p1[0]] + [p2[0]], p2[1:], c)
+  else:
+    print("Player 2 wins the round!")
+    return play_round_rec(p1[1:], p2[1:] + [p2[0]] + [p1[0]], c)
+
+
 def calc_score(cards: [], score=0, idx=1) -> int:
   if len(cards) == 0:
     return score
@@ -33,11 +70,13 @@ def solve_part_1(text: str):
 
 
 def solve_part_2(text: str):
-  pass
+  (p1, p2) = parse_cards(text)
+  winner_id, winner_deck = play_round_rec(p1, p2)
+  return calc_score(winner_deck)
 
 
 if __name__ == '__main__':
   with open("input.txt", "r") as f:
     quiz_input = f.read()
-    print("Part 1:", solve_part_1(quiz_input))
+    # print("Part 1:", solve_part_1(quiz_input))
     print("Part 2:", solve_part_2(quiz_input))
