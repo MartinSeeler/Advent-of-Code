678b2c310b6c223e3baf406d349e24169646dd38
Martin Seeler <developer@chasmo.de>
2020-12-22 06:21:32 +0100

    Add solution for day22

---
+++ 2020/day22/solution.py
@@ -0,0 +1,43 @@
+def parse_cards(text: str) -> ([], []):
+  return list(list(xs.splitlines()[1:]) for xs in text.split("\n\n"))
+
+
+def play_round(p1, p2):
+  if len(p1) == 0:
+    return 1, p2
+  if len(p2) == 0:
+    return 0, p1
+  print(f"Player 1's deck: {', '.join(p1)}")
+  print(f"Player 2's deck: {', '.join(p2)}")
+  print(f"Player 1 plays: {p1[0]}")
+  print(f"Player 2 plays: {p2[0]}")
+  if int(p1[0]) > int(p2[0]):
+    print("Player 1 wins the round!")
+    return play_round(p1[1:] + [p1[0]] + [p2[0]], p2[1:])
+  else:
+    print("Player 2 wins the round!")
+    return play_round(p1[1:], p2[1:] + [p2[0]] + [p1[0]])
+
+
+def calc_score(cards: [], score=0, idx=1) -> int:
+  if len(cards) == 0:
+    return score
+  else:
+    return calc_score(cards[:-1], score=score + (idx * int(cards[-1])), idx=idx + 1)
+
+
+def solve_part_1(text: str):
+  (p1, p2) = parse_cards(text)
+  winner_id, winner_deck = play_round(p1, p2)
+  return calc_score(winner_deck)
+
+
+def solve_part_2(text: str):
+  pass
+
+
+if __name__ == '__main__':
+  with open("input.txt", "r") as f:
+    quiz_input = f.read()
+    print("Part 1:", solve_part_1(quiz_input))
+    print("Part 2:", solve_part_2(quiz_input))
---
+++ 2020/day22/test.py
@@ -0,0 +1,33 @@
+import pytest
+
+from solution import solve_part_1, solve_part_2
+
+
+@pytest.mark.parametrize("quiz_input,expected_result", [
+  ("""\
+Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10""", 306)
+])
+def test_part_1_solution(quiz_input, expected_result):
+  assert solve_part_1(quiz_input) == expected_result
+
+
+@pytest.mark.parametrize("quiz_input,expected_result", [
+  ("""\
+      fill
+      me
+      in""", 0)
+])
+def test_part_2_solution(quiz_input, expected_result):
+  assert solve_part_2(quiz_input) == expected_result
