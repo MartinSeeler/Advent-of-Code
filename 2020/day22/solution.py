b30edc07e3239cb6c9e5f76d1a7fc20548b65bda
Martin Seeler <developer@chasmo.de>
2020-12-22 10:26:51 +0100

    Make some optimizations day22

--- 2020/day22/solution.py
+++ 2020/day22/solution.py
@@ -1,61 +1,59 @@
-from collections import Counter
-import sys
-
-sys.setrecursionlimit(100500)
+from typing import Deque, List
 
 
-def parse_cards(text: str) -> ([], []):
-  return list(list(xs.splitlines()[1:]) for xs in text.split("\n\n"))
+def parse_decks(text: str) -> [List[int], List[int]]:
+  return [list(int(x) for x in xs.splitlines()[1:]) for xs in text.split("\n\n")]
 
 
-def play_round(p1, p2):
-  if len(p1) == 0:
-    return 1, p2
-  if len(p2) == 0:
-    return 0, p1
-  if int(p1[0]) > int(p2[0]):
-    return play_round(p1[1:] + [p1[0]] + [p2[0]], p2[1:])
-  else:
-    return play_round(p1[1:], p2[1:] + [p2[0]] + [p1[0]])
-
-
-def play_round_rec(p1, p2, c=Counter()):
-  if len(p1) == 0:
-    return 1, p2
-  if len(p2) == 0:
-    return 0, p1
-  round_key = f"{', '.join(p1)}-{', '.join(p2)}"
-  if c[round_key] > 0:
-    return 0, p1
-  c[round_key] += 1
-  if (len(p1[1:]) >= int(p1[0])) and (len(p2[1:]) >= int(p2[0])):
-    winner_id, winner_deck = play_round_rec(p1[1:int(p1[0]) + 1], p2[1:int(p2[0]) + 1], Counter())
-    if winner_id:
-      return play_round_rec(p1[1:], p2[1:] + [p2[0]] + [p1[0]], c)
+def play_round_1(p1: List[int], p2: List[int]) -> (int, Deque[int]):
+  while p1 and p2:
+    h1, h2 = p1[0], p2[0]
+    p1, p2 = p1[1:], p2[1:]
+    if h1 > h2:
+      p1 += [h1, h2]
     else:
-      return play_round_rec(p1[1:] + [p1[0]] + [p2[0]], p2[1:], c)
-  elif int(p1[0]) > int(p2[0]):
-    return play_round_rec(p1[1:] + [p1[0]] + [p2[0]], p2[1:], c)
-  else:
-    return play_round_rec(p1[1:], p2[1:] + [p2[0]] + [p1[0]], c)
+      p2 += [h2, h1]
+  return (0, p1) if p1 else (1, p2)
 
 
-def calc_score(cards: [], score=0, idx=1) -> int:
-  if len(cards) == 0:
-    return score
-  else:
-    return calc_score(cards[:-1], score=score + (idx * int(cards[-1])), idx=idx + 1)
+def play_round_2(p1: List[int], p2: List[int]) -> (int, Deque[int]):
+  used = set()
+  while p1 and p2:
+    key = str(p1) + "|" + str(p2)
+    if key in used:
+      return 1, p2
+    used.add(key)
+
+    h1, h2 = p1[0], p2[0]
+    p1, p2 = p1[1:], p2[1:]
+
+    if len(p1) < h1 or len(p2) < h2:
+      if h1 > h2:
+        p1 += [h1, h2]
+      else:
+        p2 += [h2, h1]
+    else:
+      winner, _ = play_round_2(p1[:h1], p2[:h2])
+      if winner == 1:
+        p1 += [h1, h2]
+      else:
+        p2 += [h2, h1]
+  return (1, p1) if len(p1) > 0 else (2, p2)
+
+
+def calc_score(deck: List[int]) -> int:
+  return sum((i + 1) * v for i, v in enumerate(reversed(deck)))
 
 
 def solve_part_1(text: str):
-  (p1, p2) = parse_cards(text)
-  winner_id, winner_deck = play_round(p1, p2)
+  (p1, p2) = parse_decks(text)
+  _, winner_deck = play_round_1(p1, p2)
   return calc_score(winner_deck)
 
 
 def solve_part_2(text: str):
-  (p1, p2) = parse_cards(text)
-  winner_id, winner_deck = play_round_rec(p1, p2)
+  (p1, p2) = parse_decks(text)
+  _, winner_deck = play_round_2(p1, p2)
   return calc_score(winner_deck)
 
 
