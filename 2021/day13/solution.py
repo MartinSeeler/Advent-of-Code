ada5c888be71592c2201a4557f98cb2ea31b7a7c
Martin Seeler <developer@chasmo.de>
2021-12-13 07:46:35 +0100

    Cleanup

--- 2021/day13/solution.py
+++ 2021/day13/solution.py
@@ -1,30 +1,7 @@
 import time
 
 
-def solve_part_1(text: str):
-    top_section, bottom_section = text.split("\n\n")
-    dots = set()
-    for line in top_section.splitlines():
-        x, y = list(map(int, line.split(",")))
-        dots.add(complex(x, y))
-    for line in bottom_section.splitlines()[:1]:
-        axis, x = line.split(" ")[2].split("=")
-        x = int(x)
-        if axis == "y":
-            belows = set(filter(lambda d: d.imag > x, dots))
-            dots -= belows
-            for b in belows:
-                dots.add(complex(b.real, x - (b.imag - x)))
-        else:
-            rights = set(filter(lambda d: d.real > x, dots))
-            dots -= rights
-            for r in rights:
-                dots.add(complex(x - (r.real - x), r.imag))
-    return len(dots)
-
-
-def solve_part_2(text: str):
-    top_section, bottom_section = text.split("\n\n")
+def solve(top_section: str, bottom_section: str):
     dots = set()
     for line in top_section.splitlines():
         x, y = list(map(int, line.split(",")))
@@ -42,14 +19,19 @@ def solve_part_2(text: str):
             dots -= rights
             for r in rights:
                 dots.add(complex(x - (r.real - x), r.imag))
-    for y in range(0, 10):
-        s = ""
-        for x in range(0, 50):
-            if complex(x, y) in dots:
-                s += "#"
-            else:
-                s += "."
-        print(s)
+    return dots
+
+
+def solve_part_1(text: str):
+    top_section, bottom_section = text.split("\n\n")
+    return len(solve(top_section, bottom_section.split("\n")[0]))
+
+
+def solve_part_2(text: str):
+    top_section, bottom_section = text.split("\n\n")
+    dots = solve(top_section, bottom_section)
+    for y in range(0, 6):
+        print("".join(["#" if complex(x, y) in dots else " " for x in range(0, 39)]))
     return len(dots)
 
 
