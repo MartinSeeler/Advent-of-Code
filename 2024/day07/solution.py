788a875fb1c9271a45b213065423388d1d23d563
Martin Seeler <developer@chasmo.de>
2024-12-07 06:20:14 +0100

    Add day07 2024

---
+++ 2024/day07/solution.py
@@ -0,0 +1,54 @@
+import time
+
+add = lambda a, b: a + b
+multiply = lambda a, b: a * b
+merge = lambda a, b: int(str(a) + str(b))
+
+
+def can_make_target(target, numbers, ops):
+    possible_results = {numbers[0]}
+
+    for num in numbers[1:]:
+        new_results = set()
+        for prev_result in possible_results:
+            for op in ops:
+                new_results.add(op(prev_result, num))
+
+        possible_results = new_results
+
+    return target in possible_results
+
+
+def parse_input(text: str):
+    for line in text.splitlines():
+        exp_result = int(line.split(": ")[0])
+        nums = [int(x) for x in line.split(": ")[1].split(" ")]
+        yield exp_result, nums
+
+
+def solve_part_1(text: str):
+    return sum(
+        exp_result
+        for exp_result, nums in parse_input(text)
+        if can_make_target(exp_result, nums, [add, multiply])
+    )
+
+
+def solve_part_2(text: str):
+    return sum(
+        exp_result
+        for exp_result, nums in parse_input(text)
+        if can_make_target(exp_result, nums, [add, multiply, merge])
+    )
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        quiz_input = f.read()
+        start = time.time()
+        p_1_solution = int(solve_part_1(quiz_input))
+        middle = time.time()
+        print(f"Part 1: {p_1_solution} (took {(middle - start) * 1000:.3f}ms)")
+        p_2_solution = int(solve_part_2(quiz_input))
+        end = time.time()
+        print(f"Part 2: {p_2_solution} (took {(end - middle) * 1000:.3f}ms)")
---
+++ 2024/day07/test.py
@@ -0,0 +1,47 @@
+import pytest
+
+from solution import solve_part_1, solve_part_2
+
+
+@pytest.mark.parametrize(
+    "quiz_input,expected_result",
+    [
+        (
+            """\
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20""",
+            3749,
+        )
+    ],
+)
+def test_part_1_solution(quiz_input, expected_result):
+    assert solve_part_1(quiz_input) == expected_result
+
+
+@pytest.mark.parametrize(
+    "quiz_input,expected_result",
+    [
+        (
+            """\
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20""",
+            11387,
+        )
+    ],
+)
+def test_part_2_solution(quiz_input, expected_result):
+    assert solve_part_2(quiz_input) == expected_result
